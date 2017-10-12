7c1008894204a2781efa2e23a7fd9909b11aa8f9
Tom Needham <06needhamt@gmail.com>
2017-10-12 17:32:57 +0100

    Init Project

---
+++ VirusTotal-Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusTotal_Test
+{
+    class Program
+    {
+        private static readonly string API_KEY = Environment.GetEnvironmentVariable("VT_API_KEY");
+
+        static void Main(string[] args)
+        {
+            VirusTotalHandle handle = new VirusTotalHandle(API_KEY, true);
+            Console.WriteLine(handle.isInitialised());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VirusTotal-Test/VirusTotalHandle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirusTotalNET;
+
+namespace VirusTotal_Test
+{
+    class VirusTotalHandle
+    {
+        private VirusTotal handle;
+
+        public VirusTotalHandle(string API_KEY, bool useTLS)
+        {
+            handle = new VirusTotal(API_KEY);
+            handle.UseTLS = useTLS;
+        }
+
+        public bool isInitialised()
+        {
+            return handle.ApiUrl != "";
+        }
+    }
+}
